28bab837b1db7eb5d2b1505f6dab3d615fd16efa
Lee-DongHoon <51987233+LeeHoonDong@users.noreply.github.com>
2022-07-16 17:18:45 +0900

    [Silver I] Title: 단지번호붙이기, Time: 76 ms, Memory: 31084 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,30 +1,38 @@
-dx=[-1,1,0,0]
-dy=[0,0,1,-1]
-N=int(input())
-graph=[]
-count=0
-for i in range(N):
-    graph.append(list(map(int,input())))
-def dfs(i,j):
+def dfs(i,j,N,arr):
     global count
     if i<0 or i>=N or j<0 or j>=N:
         return False
-    if graph[i][j]==1:
+    if arr[i][j]!=0:
+        arr[i][j]=0
+        
         count+=1
-        graph[i][j]=0
         for index in range(4):
-            dfs(i+dx[index],j+dy[index])
+            dfs(i+dx[index],j+dy[index],N,arr)
         return True
-    return False 
-se=0
-arr=[]
-for i in range(N):
-    for j in range(N):
-        if dfs(i,j)==True:
-            se+=1
-            arr.append(count)
-        count=0
-print(se)
-arr.sort()
-for ele in arr:
-    print(ele)
+    return False
+
+def solution(arr,N):
+    global count
+    answer=[]
+    danji=0
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j]!=0:
+                count=0
+                if dfs(i,j,N,arr)==True:
+                    danji+=1
+                    answer.append(count)
+    answer.sort()
+    answer.insert(0,danji)
+    for ele in answer:
+        print(ele)
+
+if __name__=="__main__":
+    N=int(input())
+    arr=[]
+    dx=[0,0,1,-1]
+    dy=[1,-1,0,0]
+    count=0
+    for i in range(N):
+        arr.append(list(map(int,input())))
+    solution(arr,N)
