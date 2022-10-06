03d56b476bf5a73699923af14c12062cf7d35035
Lee-DongHoon <51987233+LeeHoonDong@users.noreply.github.com>
2022-10-06 23:13:30 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT animal_ins.animal_id,animal_ins.animal_type,animal_ins.name
+from animal_ins
+    join animal_outs
+    on animal_ins.animal_id=animal_outs.animal_id
+where animal_ins.sex_upon_intake!=animal_outs.sex_upon_outcome
+order by animal_id
