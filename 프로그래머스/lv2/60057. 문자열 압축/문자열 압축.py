5008412ea15434ab56123613244faaca4e58ffcd
Lee-DongHoon <51987233+LeeHoonDong@users.noreply.github.com>
2022-10-07 15:25:58 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -1,22 +1,18 @@
 def solution(s):
-    answer=len(s)
+    answer = len(s)
     for i in range(1,len(s)//2+1):
         sample=s[0:i]
-        compression=""
+        compressed_word=""
         count=1
-        for j in range(i,len(s),i):
+        for j in range(i,len(s)+i,i):
             if sample==s[j:j+i]:
                 count+=1
             else:
                 if count>=2:
-                    compression+=str(count)+sample
+                    compressed_word+=str(count)+sample
                 else:
-                    compression+=sample  
+                    compressed_word+=sample
                 sample=s[j:j+i]
                 count=1
-        if count>=2:
-            compression+=str(count)+sample
-        else:
-            compression+=sample
-        answer=min(answer,len(compression))
+        answer=min(len(compressed_word),answer)
     return answer
