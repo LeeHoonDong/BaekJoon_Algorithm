35d9c5cc7ff74cf4d687d10eec35b8064e2f6d13
LeeHoonDong <ldhoon0813@gmail.com>
2022-11-05 18:58:24 +0900

    최대동전거슬러주기 solve

---
+++ CodeTree/Dynamic Programming/최대동전거슬러주기.py
@@ -0,0 +1,22 @@
+#N개의 동전 종류, 금액 M 거슬러 주기
+#최대로 거슬러주기
+import sys
+input=sys.stdin.readline
+def solution(N,M,arr):
+    answer=0
+    dp=[-1 for _ in range(M+1)]
+    dp[0]=0
+    for i in range(1,M+1):
+        for j in range(len(arr)):
+            if i>=arr[j]:
+                if dp[i-arr[j]]==-1:
+                    continue
+                dp[i]=max(dp[i],dp[i-arr[j]]+1)
+    if dp[M]==0:
+        answer=-1
+    else:
+        answer=dp[M]
+    return answer
+N,M=map(int,input().split())
+arr=list(map(int,input().split()))
+print(solution(N,M,arr))
